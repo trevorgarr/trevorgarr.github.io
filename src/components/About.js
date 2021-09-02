4b91be051248be6b8c86d3fc354737351de3ab21
Trevor Garrood <garrood.trevor@gmail.com>
2021-09-01 23:04:24 -0700

    Style changes

--- src/components/About.js
+++ src/components/About.js
@@ -6,18 +6,17 @@ export default function About() {
       <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
         <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
           <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
-            Hi, I'm Trevor.
-            <br className="hidden lg:inline-block" />I am a student software engineer from Seattle, WA.
+            Hi! I'm Trevor.
+            <br className="hidden lg:inline-block" /> A student software engineer from Seattle, WA.
           </h1>
           <p className="mb-8 leading-relaxed">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui
-            laborum quasi, incidunt dolore iste nostrum cupiditate voluptas?
-            Laborum, voluptas natus?
+            I'm currently a senior at the University of Washington studying electrical and computer engineering. I'm
+            passionate about all things computer science and excited to join the software industry next year.
           </p>
           <div className="flex justify-center">
             <a
               href="#contact"
-              className="inline-flex text-white bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-green-600 rounded text-lg">
+              className="inline-flex text-white bg-purple-500 border-0 py-2 px-6 focus:outline-none hover:bg-purple-600 rounded text-lg">
               Work With Me
             </a>
             <a
